658fe1c80c4173614e358e0bcd09a99cb3f0c338
Levon Chilingaryan <chilingaryan1990@list.ru>
2023-01-03 13:30:24 +0400

    tas1

---
+++ script.js
@@ -0,0 +1,24 @@
+const arr = [
+    "hello",
+    234,
+    [3, 5, 9, 0],
+    {name:"john", age: 23, favorit_films: ["test", "hello", "esim"]},
+    23,
+    "Mike",
+    2345
+];
+
+
+for(let i = 0; i < arr.length; i++){
+    if(typeof arr[i] === "object" && arr[i].favorit_films){
+        
+     console.log(`film1:${arr[i].favorit_films[0]}`);
+     console.log(`film2:${arr[i].favorit_films[1]}`);
+     console.log(`film3:${arr[i].favorit_films[2]}`);  
+    }  
+    
+}
+
+
+
+
